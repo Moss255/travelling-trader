6f8f493dfffce528d025c909de9ddcac0478d676
Jack <Jackhmoss@gmail.com>
2023-10-02 18:07:46 +0100

    Added energy bar

--- src/components/counter/energy.ts
+++ src/components/counter/energy.ts
@@ -1,18 +1,24 @@
-import { Text } from "pixi.js";
+import { Sprite, Texture } from "pixi.js";
 
-class EnergyCounter extends Text {
+class EnergyCounter extends Sprite {
+    private textures: Texture[];
     constructor(private value: number) {
-        super(value.toString());
+        let textures = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => { return Texture.from(`assets/images/energybar/${value}.png`)});
+        super(textures[0]);
 
-        this.x = 0;
-        this.y = 0;
+        this.textures = textures;
+
+        this.anchor.set(0.5);
+
+        this.x = window.innerWidth / 2 - 40;
+        this.y = 30;
 
         this.visible = true;
     }
 
     update(newValue: number) {
         this.value = newValue;
-        this.text = this.value.toString();
+        this.texture = this.textures[this.value];
     }
 }
 
